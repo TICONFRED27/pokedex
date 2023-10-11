99b465f993452cf1b88e88b50cac831e3579a584
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-11 12:08:56 +0200

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,23 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+export default PokemonCard
+
+
+
+
 function PokemonCard(){
+    const pokemon = pokemonList [0]
     return (
+        
     <figure>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
         <figcaption>Bulbasaur</figcaption>
@@ -7,4 +25,4 @@ function PokemonCard(){
     )
 }
 
-export default PokemonCard
+
