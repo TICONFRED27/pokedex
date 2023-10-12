2779bf8b58fd5e657b6e2aa11e43e95155d73ca3
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-12 18:40:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 import PropTypes from "prop-types";
+
 function App() {
   const pokemonList = [
     {
@@ -12,12 +13,38 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "mew",
+      name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
     },
   ];
   const pokemon = pokemonList[0];
+
+  const [pokemonIndex , setpokemonIndex] = useState(0);
+  
+  const handleNextClick = () => {
+    setpokemonIndex(pokemonIndex +1)}
+  const handlePrevClick = () => {
+    setpokemonIndex(pokemonIndex -1)}
+  
+
   return (
     <div> <PokemonCard pokemon = {pokemon}/>
+    <button onClick={handlePrevClick}>precedent</button>
+    <button onClick={handleNextClick}>suivant</button>
 
     </div>
   )
