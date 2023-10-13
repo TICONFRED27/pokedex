deec60e03ee0d83058ae8f6cc345795fa488e09b
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-13 12:21:33 +0200

    Create NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+<>
+<button disabled={pokemonIndex <= 0} onClick={handlePrevClick}>precedent</button>
+<button disabled= {pokemonIndex >= pokemonList.length -1} onClick={handleNextClick}>suivant</button>
+</>
+
+
+
+
+
+
+
+
+
+
+
+export default App
