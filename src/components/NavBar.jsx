5d44d50fbbce00b5504f27a147fbb380aee3ca15
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-14 10:17:09 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,23 @@
+import { useState } from 'react'
 
+function NavBar({pokemonList}){
+   
+    const [pokemonIndex , setpokemonIndex] = useState(0);
+    const handleNextClick = () => {
+        setpokemonIndex(pokemonIndex +1)}
+      const handlePrevClick = () => {
+        setpokemonIndex(pokemonIndex -1)}
+      
+    return(
+        <>
+        <button disabled={pokemonIndex <= 0} onClick={handlePrevClick}>precedent</button>
+        <button disabled= {pokemonIndex >= pokemonList.length -1} onClick={handleNextClick}>suivant</button>
+        </>)
+        
+        
+        
 
+    
+}
 
-
-
-
-
-
-
-
-
-<>
-<button disabled={pokemonIndex <= 0} onClick={handlePrevClick}>precedent</button>
-<button disabled= {pokemonIndex >= pokemonList.length -1} onClick={handleNextClick}>suivant</button>
-</>
-
-
-
-
-
-
-
-
-
-
-
-export default App
+export default NavBar
